efbc0551facf6e3e610e22e0ec81491886b8c5a4
Geo <30843783+geotrice@users.noreply.github.com>
2022-01-12 21:58:00 -0600

    Create BakedGood.php
    Define the Interface class of the Factory pattern.

---
+++ Factory/BakedGood.php
@@ -0,0 +1,10 @@
+<?php
+//BakedGood.php
+//BakedGoods is a part of the Factory Pattern.
+//This is an interface class which is implemented (defined) by all Baked Goods which the factories bake.
+
+interface BakedGood
+{
+  public function getProperties();
+}
+?>
