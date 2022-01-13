2176682537bb0b2685213a5d320db097272cb2a3
Geo <30843783+geotrice@users.noreply.github.com>
2022-01-12 21:55:35 -0600

    Update CookieFactory.php
    Fix some typos.

--- Factory/CookieFactory.php
+++ Factory/CookieFactory.php
@@ -2,10 +2,12 @@
 //CookieFactory.php
 //
 include_once('Baker.php');
-include_once("ChocolateChipCookie.php);
+include_once('ChocolateChipCookie.php');
 
 class CookieFactory extends Baker
 {
+  //Define the abstract function of the Baker class.
+  //The Cookie Factory bakes cookies.
   protected function bake()
   {
     $cookie = new ChocolateChipCookie();
